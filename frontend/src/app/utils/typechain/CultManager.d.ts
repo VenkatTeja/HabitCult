db742c80b19d2120d8b9aff75d386020d5336679
Sudham Jayanthi <workwithsudham@gmail.com>
2022-02-24 20:23:52 +0530

    f removed types

--- frontend/src/app/utils/typechain/CultManager.d.ts
+++ frontend/src/app/utils/typechain/CultManager.d.ts
@@ -17,7 +17,7 @@ import {
 import { BytesLike } from "@ethersproject/bytes";
 import { Listener, Provider } from "@ethersproject/providers";
 import { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
-import type { TypedEventFilter, TypedEvent, TypedListener } from "../../../../../backend/typechain/common";
+import type { TypedEventFilter, TypedEvent, TypedListener } from "./common";
 
 interface CultManagerInterface extends ethers.utils.Interface {
   functions: {
